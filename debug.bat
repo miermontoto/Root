0a7a6c257ac2c15d60a5727a804fb75415e114c5
Juan Mier <underscoreBis@gmail.com>
2018-02-23 16:23:23 +0100

    Update debug.bat
    Standalone update that now supports gater. It shouldn't really affect if you are just using it for Root.

--- debug.bat
+++ debug.bat
@@ -1,2 +1,32 @@
+@echo off
 pushd & color & title Root Debug Tool
+if EXIST Root.cmd (
+	if EXIST gater.cmd (
+		goto both) ELSE (
+		goto Root)
+	) ELSE (
+	if EXIST gater.cmd (
+		goto gater) ELSE (
+		goto invalid)
+	)
+	
+goto invalid
+
+:Root
 cmd /k Root.cmd
+goto :eof
+
+:gater
+cmd /k gater.cmd
+goto :eof
+
+:both
+choice /c RG /m "Root or gater?"
+if %errorlevel%==1 goto Root
+if %errorlevel%==2 goto gater
+goto invalid
+
+:invalid
+echo Invalid arguments
+pause
+:eof

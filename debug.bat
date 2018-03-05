8ad494396ef7ba20be16ebc8df139ddd4b87b101
Juan Mier <underscoreBis@gmail.com>
2018-03-05 21:20:46 +0100

    restored old debug system
    gater will use its own debug

--- debug.bat
+++ debug.bat
@@ -1,32 +1,5 @@
 @echo off
 pushd & color & title Root Debug Tool
-if EXIST Root.cmd (
-	if EXIST gater.cmd (
-		goto both) ELSE (
-		goto Root)
-	) ELSE (
-	if EXIST gater.cmd (
-		goto gater) ELSE (
-		goto invalid)
-	)
-	
-goto invalid
-
-:Root
-cmd /k Root.cmd
-goto :eof
-
-:gater
-cmd /k gater.cmd
-goto :eof
-
-:both
-choice /c RG /m "Root or gater?"
-if %errorlevel%==1 goto Root
-if %errorlevel%==2 goto gater
-goto invalid
-
-:invalid
-echo Invalid arguments
-pause
-:eof
+cmd /k Root.cmd 
+choice /m "Would you like to restart?"
+if %errorlevel%==1 (start debug.bat) ELSE (EXIT)
